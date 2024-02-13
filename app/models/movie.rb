201b43efbae36c748f6b0bc260bcc6941929c13a
kareem <kareem.emad@careem.com>
2024-02-13 18:55:28 +0200

    indexing average rating calculated field

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,8 @@
 class Movie < ApplicationRecord
     has_many :reviews
 
+    # we can avoid the extra average query by using a counter + a sum of the ratings
+    # but this may grow big if we have a lot of reviews
     def update_average_rating
         update_column(:average_rating, reviews.average(:rating))
       end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_13_164255) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_13_165120) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.text "description"
@@ -22,6 +22,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_13_164255) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "average_rating"
+    t.index ["average_rating"], name: "index_movies_on_average_rating"
   end
 
   create_table "reviews", force: :cascade do |t|
